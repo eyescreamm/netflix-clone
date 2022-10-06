4a1dece396dbdb79d30b41bd794d87fd575bae6d
ic__e <yoshidashi058@gmail.com>
2022-10-06 22:04:38 +0900

    トレーラーの追加

--- src/components/Row.tsx
+++ src/components/Row.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react"
 import axios from "../axios"
 import "./Row.scss"
+import YouTube from "react-youtube"
 
 const base_url = "https://image.tmdb.org/t/p/original"
 
@@ -19,8 +20,18 @@ type Movie = {
   backdrop_path: string
 }
 
+//trailerのoption
+type Options = {
+  height: string;
+  width: string;
+  playerVars: {
+    autoplay: 0 | 1 | undefined;
+  }
+}
+
 export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
   const [movies, setMovies] = useState<Movie[]>([])
+  const [trailerUrl, setTrailerUrl] = useState<string | null>("")
 
   // fetchUrlが変わるたび発火、stateを更新
   useEffect(() => {
@@ -31,6 +42,27 @@ export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
     }
     fetchData()
   }, [fetchUrl])
+
+  const opts: Options = {
+    height: "390",
+    width: "640",
+    playerVars: {
+      // https://developers.google.com/youtube/player_parameters
+      autoplay: 1,
+    },
+  }
+
+  const handleClick = async (movie: Movie) => {
+    setTrailerUrl(null)
+    try {
+      let responce = await axios.get(`/movie/${movie.id}/videos?api_key=630dcde4f1e21c4729ed21adfabb19a5`)
+      console.log(responce)
+      setTrailerUrl(responce.data.results[0]?.key ?? null)
+    } catch (error) {
+      console.error(error)
+    }
+  }
+
 
   return (
     <div className="Row">
@@ -44,9 +76,11 @@ export const Row = ({ title, fetchUrl, isLargeRow }: Props) => {
               isLargeRow ? movie.poster_path : movie.backdrop_path
             }`}
             alt={movie.name}
+            onClick={() => handleClick(movie)}
           />
         ))}
       </div>
+      {trailerUrl && <YouTube videoId={trailerUrl} opts={opts} />}
     </div>
   )
 }
